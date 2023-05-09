4800c130b5df4f56abc03b4b9885316309baed22
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2023-05-10 00:51:45 +0200

    Problema 336

---
+++ eclipse/ProblemasPropuestos/src/es/claradelrey/aceptaelreto/problema336tiraafloja/Problema336TiraAfloja.java
@@ -0,0 +1,53 @@
+package es.claradelrey.aceptaelreto.problema336tiraafloja;
+
+import java.util.Scanner;
+
+public class Problema336TiraAfloja {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		while (scanner.hasNext()) {
+			int numAlumnos = scanner.nextInt();
+			int[] pesoAlumnos = new int[numAlumnos];
+			int pesoTotalAlumnos = 0;
+			for (int i = 0; i < pesoAlumnos.length; i++) {
+				int pesoUnAlumno = scanner.nextInt();
+				pesoAlumnos[i] = pesoUnAlumno;
+				pesoTotalAlumnos += pesoUnAlumno;
+			}
+
+			// Procesamos el caso
+			procesarCaso(pesoAlumnos, pesoTotalAlumnos);
+		}
+
+	}
+
+	private static void procesarCaso(int[] pesoAlumnos, int pesoTotalAlumnos) {
+		int pesoIzquierda = 0;
+		int pesoDerecha = pesoTotalAlumnos;
+		int diferenciaMinima = pesoTotalAlumnos;
+		int cantidadAlumnosIzquierdaDiferenciaMinima = 0;
+		int pesoIzquierdaDiferenciaMinima = 0;
+
+		for (int i = 0; i < pesoAlumnos.length; i++) {
+			pesoIzquierda += pesoAlumnos[i];
+			pesoDerecha -= pesoAlumnos[i];
+			int diferencia = Math.abs(pesoIzquierda - pesoDerecha);
+			if (diferencia == diferenciaMinima) {
+				System.out.println("NO JUEGAN");
+				return;
+			}
+			if (diferencia < diferenciaMinima) {
+				diferenciaMinima = diferencia;
+				cantidadAlumnosIzquierdaDiferenciaMinima = i + 1;
+				pesoIzquierdaDiferenciaMinima = pesoIzquierda;
+			}
+		}
+		System.out.printf("%d %d %d\n", cantidadAlumnosIzquierdaDiferenciaMinima, pesoIzquierdaDiferenciaMinima,
+				pesoTotalAlumnos - pesoIzquierdaDiferenciaMinima);
+
+	}
+
+}

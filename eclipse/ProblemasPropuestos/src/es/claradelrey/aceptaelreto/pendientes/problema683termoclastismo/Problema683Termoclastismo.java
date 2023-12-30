da003138f05a3e811ea75c8bf0fc47b17a3b565d
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2023-12-30 03:09:46 +0100

    Problemas resueltos. Reorganización de paquetes y clases.

---
+++ eclipse/ProblemasPropuestos/src/es/claradelrey/aceptaelreto/pendientes/problema683termoclastismo/Problema683Termoclastismo.java
@@ -0,0 +1,56 @@
+package es.claradelrey.aceptaelreto.pendientes.problema683termoclastismo;
+
+import java.util.Scanner;
+
+public class Problema683Termoclastismo {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int numMuestras;
+		while ((numMuestras = scanner.nextInt()) != 0) {
+			procesarCaso(numMuestras);
+		}
+	}
+
+	private static void procesarCaso(int numMuestras) {
+		int menor, primeraPosicionMenor, ultimaPosicionMenor;
+		int mayor, primeraPosicionMayor, ultimaPosicionMayor;
+
+		// Asumimos que el primero es el mayor y el menor a la vez, y ya hemos procesado
+		// una muestra.
+		menor = mayor = scanner.nextInt();
+		primeraPosicionMenor = 0;
+		ultimaPosicionMenor = 0;
+		primeraPosicionMayor = 0;
+		ultimaPosicionMayor = 0;
+
+		for (int posicion = 1; posicion < numMuestras; posicion++) {
+			int temperatura = scanner.nextInt();
+
+			if (temperatura < menor) {
+				menor = temperatura;
+				primeraPosicionMenor = posicion;
+				ultimaPosicionMenor = posicion;
+			} else if (temperatura == menor) {
+				ultimaPosicionMenor = posicion;
+			}
+
+			if (temperatura > mayor) {
+				mayor = temperatura;
+				primeraPosicionMayor = posicion;
+				ultimaPosicionMayor = posicion;
+			} else if (temperatura == mayor) {
+				ultimaPosicionMenor = posicion;
+			}
+		}
+
+		int menorDistancia = Math.abs(primeraPosicionMenor - primeraPosicionMayor);
+		menorDistancia = Math.min(menorDistancia, Math.abs(primeraPosicionMenor - ultimaPosicionMayor));
+		menorDistancia = Math.min(menorDistancia, Math.abs(ultimaPosicionMenor - primeraPosicionMayor));
+		menorDistancia = Math.min(menorDistancia, Math.abs(ultimaPosicionMenor - ultimaPosicionMayor));
+
+		System.out.printf("%s %s %s\n", menor, mayor, menorDistancia);
+	}
+}
---
+++ eclipse/ProblemasPropuestos/src/es/claradelrey/aceptaelreto/resueltos/problema188encadenandopalabras/Problema188EncadenandoPalabras.java
@@ -0,0 +1,35 @@
+package es.claradelrey.aceptaelreto.resueltos.problema188encadenandopalabras;
+
+import java.util.Scanner;
+
+/**
+ * Versión A. Usa String y los métodos de la clase String.
+ */
+public class Problema188EncadenandoPalabras {
+
+	private static Scanner scanner = new Scanner(System.in);
+//	private static Scanner scanner = new Scanner("gugutata\n" + "mata tapa papa pato\n" + "seto taco coma matute\n"
+//			+ "sien encima mapa patuco comida\n" + "cata tasama malote tejaba batama\n" + "kiosko comida");
+
+	public static void main(String[] args) {
+
+		while (scanner.hasNextLine()) {
+			String linea = scanner.nextLine();
+			String[] palabras = linea.split(" ");
+			boolean sonEncadenadas = true;
+			// El array lo recorremos hasta la penúltima posición. Al acceder a i+1, si lo
+			// recorrierámos hasta la última posición lanzaría una excepción.
+			for (int i = 0; i < palabras.length - 1 && sonEncadenadas; i++) {
+				// En cada iteración comprobamos si una palabra (en posición i+1) es la palabra
+				// encadenada de la palabra en la posición anterior (posición i)
+				sonEncadenadas = sonEncadenadas(palabras[i], palabras[i + 1]);
+			}
+			System.out.println(sonEncadenadas ? "SI" : "NO");
+		}
+
+	}
+
+	private static boolean sonEncadenadas(String anterior, String posterior) {
+		return anterior.substring(anterior.length() - 2).equals(posterior.substring(0, 2));
+	}
+}
